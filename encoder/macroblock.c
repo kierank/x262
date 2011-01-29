kierank/x262

commit 09077e52b214d5a5cffc49fd0a24da68fac9a176

Inter bitstream fixes f_codes and junk motion vectors are hardcoded for now

encoder/macroblock.c
@@ -1006,11 +1006,12 @@ void x264_macroblock_encode( x264_t *h )
             x264_predict_lossless_8x8_chroma( h, i_mode );
         else if( MPEG2 )
         {
+            h->mb.i_cbp_chroma = 0;
             h->predict_mpeg2_8x8( h->mb.pic.p_fdec[1], 0 );
             h->predict_mpeg2_8x8( h->mb.pic.p_fdec[2], 0 );
 
             for( int i = 4; i < 6; i++ )
-                x262_mb_encode_i_block( h, i, i_qp ); /* encode intra block */
+                x262_mb_encode_i_block( h, i, i_qp ); /* encode intra chroma */
         }
         else
         {
@@ -1018,14 +1019,18 @@ void x264_macroblock_encode( x264_t *h )
             h->predict_8x8c[i_mode]( h->mb.pic.p_fdec[2] );
         }
     }
-
     /* encode the 8x8 blocks */
-    if( MPEG2 )
-    {
-        // TODO inter MPEG-2
-    }
     else
-        x264_mb_encode_8x8_chroma( h, !IS_INTRA( h->mb.i_type ), h->mb.i_chroma_qp );
+    {
+        if( MPEG2 )
+        {
+            h->mb.i_cbp_chroma = 0;
+            for( int i = 4; i < 6; i++ )
+                x262_mb_encode_inter_block( h, i, i_qp ); /* encode inter chroma */
+        }
+        else
+            x264_mb_encode_8x8_chroma( h, !IS_INTRA( h->mb.i_type ), h->mb.i_chroma_qp );
+    }
 
     /* store cbp */ // FIXME do we need to make this into mpeg-2 cbp
     int cbp = h->mb.i_cbp_chroma << 4 | h->mb.i_cbp_luma;

common/common.h
@@ -672,7 +672,6 @@ struct x264_t
         int     i_dct_dc_diff[6];
         int16_t mvp[2][2];
 
-
         int     i_cbp_luma;
         int     i_cbp_chroma;
 

common/frame.h
@@ -96,6 +96,7 @@ typedef struct x264_frame
     int16_t (*mv[2])[2];
     int16_t (*mv16x16)[2];
     int16_t (*lowres_mvs[2][X264_BFRAME_MAX+1])[2];
+    int16_t mv_fcode[2][2]; /* MPEG-2 */
 
     /* Stored as (lists_used << LOWRES_COST_SHIFT) + (cost).
      * Doesn't need special addressing for intra cost because

encoder/mpeg2vlc.c
@@ -43,13 +43,17 @@ static void x262_write_dct_vlcs( x264_t *h, dctcoef *l, int intra_tab )
     if( runlevel.run[i_total] && h->mb.i_type == I_16x16)
         runlevel.run[i_total]--; // compensate for the DC coefficient set to zero
 
-    for( int i = i_total; i >= 0 && runlevel.last > 0 ; i-- )
+    for( int i = i_total; i >= 0 && ( runlevel.last > 0 || h->mb.i_type != I_16x16 ); i-- )
     {
         if( abs( runlevel.level[i] ) <= 40 && runlevel.run[i] <= dct_vlc_largest_run[abs( runlevel.level[i] )] )
         {
-            bs_write_vlc( s, dct_vlcs[intra_tab][abs( runlevel.level[i] )][runlevel.run[i]] );
+            /* special case in table B.14 for abs(DC) == 1 */
+            if( !intra_tab && i == i_total && runlevel.run[i] == 0
+                && ( runlevel.level[i] == 1 || runlevel.level[i] == -1 ) )
+                bs_write1( s, 1 );
+            else
+                bs_write_vlc( s, dct_vlcs[intra_tab][abs( runlevel.level[i] )][runlevel.run[i]] );
             bs_write1( s, runlevel.level[i] < 0 );
-            if( runlevel.level[i] == 0 ) printf("\n\nbad level!\n\n");
         }
         else
         {
@@ -67,7 +71,7 @@ void x262_macroblock_write_vlc( x264_t *h )
 {
     bs_t *s = &h->out.bs;
     const int i_mb_type = h->mb.i_type;
-    int cbp, quant, mc, r_size;
+    int mc[2], r_size[2];
 
 #if RDO_SKIP_BS
     s->i_bits_encoded = 0;
@@ -76,18 +80,20 @@ void x262_macroblock_write_vlc( x264_t *h )
     int       i_mb_pos_tex = 0;
 #endif
 
-    cbp = h->mb.i_cbp_luma << 2 | h->mb.i_cbp_chroma; // coded block pattern
-    quant = h->mb.i_last_qp != h->mb.i_qp;
-    mc = 1; // FIXME
-    r_size = 6; // f_code[s][t] - 1 FIXME
+    mc[0] = mc[1] = 1; // FIXME
+
+    int cbp = h->mb.i_cbp_luma << 2 | h->mb.i_cbp_chroma; // coded_block_pattern_420
+    int quant = h->mb.i_last_qp != h->mb.i_qp;
+    int mcoded = mc[0] || mc[1];
+    r_size[0] = h->fenc->mv_fcode[0][0] - 1;
+    r_size[1] = h->fenc->mv_fcode[0][1] - 1;
 
     // macroblock modes
     if( i_mb_type == I_16x16 )
         bs_write_vlc( s, x262_i_mb_type[h->sh.i_type][quant] );
     else if( i_mb_type == P_L0 )
     {
-        bs_write_vlc( s, x262_p_mb_type[mc][!!cbp][quant] );
-        bs_write( s , 2, h->param.b_interlaced ? 1 : 2 ); // frame_motion_type
+        bs_write_vlc( s, x262_p_mb_type[mcoded][!!cbp][quant] );
     }
     else //if( i_mb_type == B_8x8 )
     {
@@ -97,15 +103,15 @@ void x262_macroblock_write_vlc( x264_t *h )
     if( quant )
         bs_write( s, 5, h->mb.i_qp ); // quantizer_scale_code
 
-    /* r = 1st or 2nd mv in macroblock
-       s = forward or backward
-       t = horizontal or vertical */
-
     // forward mvs
-    if( i_mb_type == P_L0 )
+    if( i_mb_type == P_L0 && mcoded )
     {
-        bs_write_vlc( s, x262_motion_code[mc+16] ); // motion_code
-        bs_write( s, r_size, 3 ); // motion_residual FIXME
+        for( int i = 0; i < 2; i++ )
+        {
+            bs_write_vlc( s, x262_motion_code[mc[i] + 16] ); // motion_code
+            if( mc[i] )
+                bs_write( s, r_size[i], 3+i ); // motion_residual FIXME
+        }
     }
     // backward mvs
 
@@ -135,8 +141,6 @@ void x262_macroblock_write_vlc( x264_t *h )
         }
         else if( cbp & (1<<(5-i)) )
         {
-            h->dct.mpeg2_8x8[i][0] = 100; // junk
-            h->dct.mpeg2_8x8[i][1] = 10; // more junk
             x262_write_dct_vlcs( h, h->dct.mpeg2_8x8[i], 0 );
             bs_write_vlc( s, dct_vlcs[0][0][0] ); // end of block
         }

encoder/set.c
@@ -782,10 +782,23 @@ void x262_pic_coding_extension_write( x264_t *h, bs_t *s )
     bs_realign( s );
 
     bs_write( s, 4, MPEG2_PIC_CODING_EXT_ID ); // extension_start_code_identifier
+
+    h->fenc->mv_fcode[0][0] = h->fenc->mv_fcode[0][1] = 7; // FIXME
+    // f_code[s][t]
     if( ( h->sps->i_profile_idc == MPEG2_PROFILE_SIMPLE && !h->param.i_bframe ) || IS_X264_TYPE_I( h->fenc->i_type ) )
-        bs_write( s, 16, 0xffff ); // f_code[s][t]
+        bs_write( s, 16, 0xffff );
+    else if( h->fenc->i_type == X264_TYPE_P )
+    {
+        for( int i = 0; i < 2; i++ )
+            bs_write( s, 4, h->fenc->mv_fcode[0][i] );
+        bs_write( s, 8, 0xff );
+    }
     else
-        bs_write( s, 16, 0x7700 ); // FIXME
+    {
+        for( int j = 0; j < 2; j++ )
+            for( int i = 0; i < 2; i++ )
+                bs_write( s, 4, h->fenc->mv_fcode[j][i] );
+    }
     bs_write( s, 2, h->param.i_intra_dc_precision ); // intra_dc_precision
     bs_write( s, 2, !h->param.b_interlaced ? 3 : h->param.b_tff ? 1 : 2 ); // picture_structure
     bs_write1( s, h->param.b_interlaced ? h->param.b_tff : 0 ); // top_field_first

commit f1bac35838b6f9e15090c80f26291946f41f7a9b

Fix regression in H.264 Bug introduced in 09077e52b214d5a5cffc

encoder/macroblock.c
@@ -1019,17 +1019,14 @@ void x264_macroblock_encode( x264_t *h )
         }
     }
     /* encode the 8x8 blocks */
-    else
+    if( MPEG2 )
     {
-        if( MPEG2 )
-        {
-            h->mb.i_cbp_chroma = 0;
-            for( int i = 4; i < 6; i++ )
-                x262_mb_encode_inter_block( h, i, i_qp ); /* encode inter chroma */
-        }
-        else
-            x264_mb_encode_8x8_chroma( h, !IS_INTRA( h->mb.i_type ), h->mb.i_chroma_qp );
+        h->mb.i_cbp_chroma = 0;
+        for( int i = 4; i < 6; i++ )
+            x262_mb_encode_inter_block( h, i, i_qp ); /* encode inter chroma */
     }
+    else
+        x264_mb_encode_8x8_chroma( h, !IS_INTRA( h->mb.i_type ), h->mb.i_chroma_qp );
 
     /* store cbp */ // FIXME do we need to make this into mpeg-2 cbp
     int cbp = h->mb.i_cbp_chroma << 4 | h->mb.i_cbp_luma;