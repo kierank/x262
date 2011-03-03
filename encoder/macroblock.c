kierank/x262

commit e004fbfca28eae7a835f3eb1aaad23a3018962aa

Redo MC whenever analysis chooses skip for an MB that can't be skipped

encoder/macroblock.c
@@ -733,6 +733,10 @@ void x264_macroblock_encode( x264_t *h )
             h->mb.i_type = P_L0;
         else if( h->mb.i_type == B_SKIP )
             h->mb.i_type = h->mb.i_mb_type_left;
+        /* Do motion compensation if we have a non-zero mv */
+        if( h->mb.cache.mv[0][x264_scan8[0]][0] | h->mb.cache.mv[0][x264_scan8[0]][1] |
+            h->mb.cache.mv[1][x264_scan8[0]][0] | h->mb.cache.mv[1][x264_scan8[0]][1] )
+            x264_mb_mc( h );
     }
 
     if( h->mb.i_type == P_SKIP )

commit 17fce83edf62edc48a8e40c754838fd2833631df

Fix crash with I frames Regression in e004fbfca28eae7a835f3eb1aaad23a3018962aa

encoder/macroblock.c
@@ -735,7 +735,8 @@ void x264_macroblock_encode( x264_t *h )
             h->mb.i_type = h->mb.i_mb_type_left;
         /* Do motion compensation if we have a non-zero mv */
         if( h->mb.cache.mv[0][x264_scan8[0]][0] | h->mb.cache.mv[0][x264_scan8[0]][1] |
-            h->mb.cache.mv[1][x264_scan8[0]][0] | h->mb.cache.mv[1][x264_scan8[0]][1] )
+            h->mb.cache.mv[1][x264_scan8[0]][0] | h->mb.cache.mv[1][x264_scan8[0]][1] &&
+            h->sh.i_type != SLICE_TYPE_I )
             x264_mb_mc( h );
     }
 